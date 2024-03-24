c42d8535b994412074740e24e71ce9cc3d09e2e5
Saahith Janapati <itzsaahith01@gmail.com>
2024-03-24 05:16:28 -0400

    switching to linux machine

--- python_stuff/main.py
+++ python_stuff/main.py
@@ -4,10 +4,20 @@ from tello_controller import TelloController
 import sys
 import cv2
 import base64 
+from trip_writer import write_to_trip, add_to_trip
+import os
 
 # tello_controller = TelloController()
 
 def main():
+    # delete notes.txt and picture.png from trip folder
+    if os.path.exists("trip/notes.txt"):
+        os.remove("trip/notes.txt")
+
+    if os.path.exists("trip/picture.png"):
+        os.remove("trip/picture.png")
+
+
     modality_type = input("Enter modality type: ")
     if modality_type == "voice":
         voice = VoiceModule()
@@ -15,6 +25,8 @@ def main():
         while True:
             commands_json = voice.run_voice_step()
             print(commands_json)
+            
+            add_to_trip(f"```\n{commands_json}\n```")
             # run_commands(commands_json)
     
     elif modality_type == "gesture":
@@ -26,13 +38,18 @@ def main():
             ret, frame = cap.read()
             cv2.imshow('Webcam Feed', frame)
 
-            file_name = "images/picture.png"
-            
+            file_name = "trip/picture.png"
             cv2.imwrite(file_name, frame)
 
             img_base64 = encode_img_base64(frame)
             commands_str = gesture.process_frame(img_base64)
+
+
+            commands_arr = commands_str.split(",")
+            add_to_trip(f"```{commands_arr[0]}(''.join({commands_arr[1:]}))```")
             print(commands_str)
+
+
             # run_commands_str(commands_str)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
--- python_stuff/streamlit_app2.py
+++ python_stuff/streamlit_app2.py
@@ -10,24 +10,25 @@ def read_and_format_content(file_path):
     # For simplicity, let's assume the content is already in Markdown format
     return content
 
-
 def main():
-    # Path to the folder where the content is being written
-    # Placeholder to display content
-    content_placeholder = st.empty()
+    st.markdown("# Flight Log")
+    content_placeholder = st.empty()  # Create a placeholder for content
 
     while True:
-        # Read content from file
+        # Clear the content of the app
+        content_placeholder.empty()
+
+        if os.path.exists("trip/picture.png"):
+            content_placeholder.image("trip/picture.png")
+
         file_path = os.path.join('trip/', "notes.txt")
         if os.path.exists(file_path):
             content = read_and_format_content(file_path)
             # Display formatted content
             content_placeholder.markdown(content)
 
-        st.image('test_files/palm.jpeg')
-
-        # Wait for 2 seconds before updating again
-        time.sleep(0.5)
+        # Wait for 1 second before updating again
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
